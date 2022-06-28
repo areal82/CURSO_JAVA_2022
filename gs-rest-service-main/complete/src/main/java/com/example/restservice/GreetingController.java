da3765115c5ac8b903503f8e158ddda4801d1ca2
Miguel Angel Real Gomez <areal@atsistemas.com>
2022-06-28 15:01:03 +0200

    Ejemplo JPA

--- gs-rest-service-main/complete/src/main/java/com/example/restservice/GreetingController.java
+++ gs-rest-service-main/complete/src/main/java/com/example/restservice/GreetingController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,7 @@ public class GreetingController {
 	private AtomicLong counter;
 
 	@GetMapping("/greeting")
+	// @RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World", required = false) String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
@@ -40,6 +43,7 @@ public class GreetingController {
 	}
 	
 	@PostMapping(value = "/createWithRS", produces = MediaType.APPLICATION_JSON_VALUE)
+	// @RequestMapping(method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Greeting> createGreetingWithRS(
 	  @RequestBody Greeting newGreeting) {
 		return ResponseEntity.ok(newGreeting);
--- gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.servingwebcontent;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +11,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+	@Value( "${greeting.spanish}" )
+	private String defaultGreeting;
+	
+//	@GetMapping("/greeting")
+//	public String greeting(@RequestParam(name="name", 
+//		required=true, defaultValue="World") String name,
+//			Model model) {
+//		model.addAttribute("name", name);
+//		return "greeting";
+//	}
+	
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", 
-		required=true, defaultValue="World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+	public String greeting(@RequestParam(name="name", required=false) String name, Model model) {
+		if(Objects.nonNull(name)) {
+			model.addAttribute("name", name);
+		}
+		else {
+			model.addAttribute("name", defaultGreeting);
+		}
 		return "greeting";
 	}
-
 }
